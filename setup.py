9617ce2defb6560027ad91f2ed93beeb3afb95a3
Mike Moore <z_z_zebra@yahoo.com>
2022-04-20 17:38:11 +0100

    Widened the range of versions of google-api-python-client and google-cloud-storage the module depends upon to include 1.0 and 2.0 versions not just 2.0

--- setup.py
+++ setup.py
@@ -44,8 +44,8 @@ setuptools.setup(
     scripts=[],
     install_requires=[
         "google-cloud-secret-manager~=2.0",
-        "google-api-python-client~=2.0",
-        "google-cloud-storage~=2.0",
+        "google-api-python-client>1.0<3.0",
+        "google-cloud-storage>1.0,<3.0",
         "grpcio~=1.0",
         "python-dateutil~=2.0",
         "pytz>=2022.0"
