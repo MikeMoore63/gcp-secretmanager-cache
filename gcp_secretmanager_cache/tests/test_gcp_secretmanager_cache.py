946a72ee04e6e75069e371695a5b01875f0dbebd
Mike Moore <mike.moore@hsbc.com>
2025-12-08 21:17:58 +0000

    feat: add annotation based test case to prove annotations work for config

--- gcp_secretmanager_cache/tests/test_gcp_secretmanager_cache.py
+++ gcp_secretmanager_cache/tests/test_gcp_secretmanager_cache.py
@@ -49,7 +49,8 @@ def setup_module():
         "SECRET_ENABLE_THEN_DISABLE",
         "SECRET_ENABLE_THEN_DISABLE_PAUSE",
         "SECRET_2_VERSION_PAUSE",
-        "TEST_SIMPLE_DECORATOR" "TEST_DECORATOR_KEYWORD",
+        "TEST_SIMPLE_DECORATOR",
+        "TEST_DECORATOR_KEYWORD",
         "TEST_NOSECRET_TOSECRET",
         "TEST_NOSECRET_TOSECRET_PAUSE",
         "TEST_SECRET_THEN_NOSECRET",
@@ -57,6 +58,9 @@ def setup_module():
         "TEST_SECRET_PERF_KEY",
         "TEST_SECRET_VERSION",
         "TEST_SECRET_ROTATION_FRAMEWORKS",
+        "TEST_SAKEY_ROTATION_FRAMEWORKS",
+        "TEST_SAKEY_ROTATION_AN_FRAMEWORKS",
+        "TEST_APIKEY_ROTATION_FRAMEWORKS",
     ]:
         TestScannerMethods.delete_secret(project_id, secret_id)
     faulthandler.register(
@@ -873,6 +877,73 @@ class TestScannerMethods(unittest.TestCase):
                             "config_bucket": bucket_name,
                             "config_object": "test-sakey",
                         },
+                        "rotation": {
+                            "rotation_period": timedelta(seconds=3600),
+                            "next_rotation_time": datetime.now(timezone.utc)
+                            + timedelta(seconds=3600),
+                        },
+                        "topics": [topic],
+                    },
+                }
+            )
+
+        sm_service = build("secretmanager", "v1")
+        secret_req = sm_service.projects().secrets().get(name=name)
+        secret_response = secret_req.execute()
+        data = json.dumps(secret_response).encode("utf-8")
+
+        rotator_mechanic = SAKeyRotator()
+        test_rotator = SecretRotator(rotator_mechanic)
+        test_rotator.rotate_secret(
+            {"eventType": "SECRET_ROTATE", "secretId": name}, data
+        )
+
+        secret_cache = GCPCachedSecret(name)
+        secret = json.loads(secret_cache.get_secret().decode("utf-8"))
+        logging.getLogger(__name__).info(f"API key secret 1 is {json.dumps(secret)}")
+        test_rotator.rotate_secret(
+            {"eventType": "SECRET_ROTATE", "secretId": name}, data
+        )
+        secret_cache.invalidate_secret()
+        secret2 = json.loads(secret_cache.get_secret().decode("utf-8"))
+        logging.getLogger(__name__).info(f"API key secret 2 is {json.dumps(secret2)}")
+        assert (
+            secret["private_key"] != secret2["private_key"]
+        ), "Initial key and second key are not the same"
+
+    def test_sa_key_rotation_annotations(self):
+        # Build the parent name from the project.
+        parent = f"projects/{self.project_id}"
+        secret_id = "TEST_SAKEY_ROTATION_AN_FRAMEWORKS"
+
+        name = self.client.secret_path(self.project_id, secret_id)
+
+        exists = True
+        try:
+            response = self.client.get_secret(request={"name": name})
+        except exceptions.NotFound as e:
+            exists = False
+
+        topic = secretmanager_v1.Topic()
+        topic.name = os.getenv("TOPIC", "projects/methodical-bee-162815/topics/foo")
+
+        config = json.dumps(
+            {
+                "name": "projects/methodical-bee-162815/serviceAccounts/test-rotate@methodical-bee-162815.iam.gserviceaccount.com"
+            }
+        )
+
+        if not exists:
+            response = self.client.create_secret(
+                request={
+                    "parent": parent,
+                    "secret_id": secret_id,
+                    "secret": {
+                        "replication": {"automatic": {}},
+                        "labels": {
+                            "secret_type": "google-serviceaccount",
+                        },
+                        "annotations": {"config": config},
                         "rotation": {
                             "rotation_period": timedelta(seconds=3600),
                             "next_rotation_time": datetime.now(timezone.utc)
